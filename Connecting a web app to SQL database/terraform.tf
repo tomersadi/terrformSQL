39b9f1cd9059256c3d590d4c749694a659ed0e43
g_tsadi <g_tsadi@icc.co.il>
2025-01-10 19:38:37 +0200

    remove sertificate

--- Connecting a web app to SQL database/terraform.tf
+++ Connecting a web app to SQL database/terraform.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id = "f300f5aa-36bf-454c-866b-41cfa2d8f15b"
-  client_secret = "kdc8Q~RZd~oGAmYvERMLgpJkMeEFBGDTL5WVpdco"
-  tenant_id = "38dbefc3-d57f-4955-b62c-1406e16a4ea8"
-  subscription_id = "387407e5-94af-45e7-b378-4d37af61c732"
+  client_id = "yyy"
+  client_secret = "xxx"
+  tenant_id = "zzz"
+  subscription_id = "qqq"
 }
